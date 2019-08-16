3bae285cc639d62621f1df07a0f7132833ccdd13
iuserea <diearborn@gmail.com>
2019-08-16 19:41:38 +0800

    1.0新窗口调用摄像头

---
+++ src/test/java/cn/yd/springboot/JavaCVCameraTest.java
@@ -0,0 +1,34 @@
+package cn.yd.springboot;
+
+
+import javax.swing.JFrame;
+
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.FrameGrabber.Exception;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+
+public class JavaCVCameraTest {
+
+
+    public static void main(String[] args) throws Exception, InterruptedException
+    {
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        grabber.start();   //开始获取摄像头数据
+        CanvasFrame canvas = new CanvasFrame("摄像头");//新建一个窗口
+        canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        canvas.setAlwaysOnTop(true);
+
+        while(true)
+        {
+            if(!canvas.isDisplayable())
+            {//窗口是否关闭
+                grabber.stop();//停止抓取
+                System.exit(2);//退出
+            }
+            canvas.showImage(grabber.grab());//获取摄像头图像并放到窗口上显示， 这里的Frame frame=grabber.grab(); frame是一帧视频图像
+
+            Thread.sleep(50);//50毫秒刷新一次图像
+        }
+    }
+}
