3348c6ac88837f5fb4b0cb7519e83444bac8b0c7
JokerZ <49329571+zhangyaa2019@users.noreply.github.com>
2019-08-16 13:53:02 +0800

    Add files via upload

--- badminton.sql
+++ badminton.sql
@@ -11,7 +11,7 @@
  Target Server Version : 60005
  File Encoding         : 65001
 
- Date: 16/08/2019 11:07:04
+ Date: 16/08/2019 13:52:29
 */
 
 SET NAMES utf8mb4;
@@ -23,12 +23,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `administrator`;
 CREATE TABLE `administrator`  (
   `administrator_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
-  `password` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
-  `name` varchar(20) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
-  `phonenumber` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `phonenumber` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`administrator_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of administrator
@@ -43,15 +43,15 @@ CREATE TABLE `appraisal`  (
   `appraisal_ID` int(11) NOT NULL AUTO_INCREMENT,
   `user_ID` int(11) NULL DEFAULT NULL,
   `area_ID` int(11) NULL DEFAULT NULL,
-  `info` text CHARACTER SET gbk COLLATE gbk_chinese_ci NULL,
+  `info` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `app_status` int(2) NULL DEFAULT NULL,
   `app_time` date NULL DEFAULT NULL,
   PRIMARY KEY (`appraisal_ID`) USING BTREE,
   INDEX `用户ID外键`(`user_ID`) USING BTREE,
-  INDEX `场地ID外键`(`area_ID`) USING BTREE,
+  INDEX `场地外键`(`area_ID`) USING BTREE,
   CONSTRAINT `用户ID外键` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `场地ID外键` FOREIGN KEY (`area_ID`) REFERENCES `area` (`area_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+  CONSTRAINT `场地外键` FOREIGN KEY (`area_ID`) REFERENCES `area` (`area_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for appraisalpic
@@ -60,11 +60,11 @@ DROP TABLE IF EXISTS `appraisalpic`;
 CREATE TABLE `appraisalpic`  (
   `apppic_ID` int(11) NOT NULL AUTO_INCREMENT,
   `appraisal_ID` int(11) NULL DEFAULT NULL,
-  `pic` varchar(512) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `pic` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`apppic_ID`) USING BTREE,
   INDEX `评价ID外键`(`appraisal_ID`) USING BTREE,
   CONSTRAINT `评价ID外键` FOREIGN KEY (`appraisal_ID`) REFERENCES `appraisal` (`appraisal_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for area
@@ -72,18 +72,18 @@ CREATE TABLE `appraisalpic`  (
 DROP TABLE IF EXISTS `area`;
 CREATE TABLE `area`  (
   `area_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `areaname` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
-  `price` decimal(10, 1) NULL DEFAULT NULL,
-  `des` text CHARACTER SET gbk COLLATE gbk_chinese_ci NULL,
-  `address` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `areaname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  `des` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `starttime` int(3) NULL DEFAULT NULL,
-  `stoptime` int(3) NULL DEFAULT NULL,
-  `profit` decimal(10, 1) NULL DEFAULT NULL,
-  `good_num` int(10) NULL DEFAULT NULL,
-  `bad_num` int(10) NULL DEFAULT NULL,
-  `good_rate` int(5) NULL DEFAULT NULL,
+  `stoptime` int(11) NULL DEFAULT NULL,
+  `profit` decimal(10, 0) NULL DEFAULT NULL,
+  `good_num` int(3) NULL DEFAULT NULL,
+  `bad_num` int(3) NULL DEFAULT NULL,
+  `good_rate` int(3) NULL DEFAULT NULL,
   PRIMARY KEY (`area_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact STORAGE DISK;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for areapic
@@ -92,11 +92,11 @@ DROP TABLE IF EXISTS `areapic`;
 CREATE TABLE `areapic`  (
   `areapic_ID` int(11) NOT NULL AUTO_INCREMENT,
   `area_ID` int(11) NULL DEFAULT NULL,
-  `pic` varchar(512) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `pic` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`areapic_ID`) USING BTREE,
-  INDEX `场地ID外键1`(`area_ID`) USING BTREE,
-  CONSTRAINT `场地ID外键1` FOREIGN KEY (`area_ID`) REFERENCES `area` (`area_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+  INDEX `场地外键1`(`area_ID`) USING BTREE,
+  CONSTRAINT `场地外键1` FOREIGN KEY (`area_ID`) REFERENCES `area` (`area_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for reservation
@@ -112,10 +112,10 @@ CREATE TABLE `reservation`  (
   `pre_status` int(2) NULL DEFAULT NULL,
   PRIMARY KEY (`reservation_ID`) USING BTREE,
   INDEX `会员ID外键1`(`user_ID`) USING BTREE,
-  INDEX `场地ID外键2`(`area_ID`) USING BTREE,
-  CONSTRAINT `场地ID外键2` FOREIGN KEY (`area_ID`) REFERENCES `area` (`area_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `会员ID外键1` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+  INDEX `场地外键2`(`area_ID`) USING BTREE,
+  CONSTRAINT `会员ID外键1` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `场地外键2` FOREIGN KEY (`area_ID`) REFERENCES `area` (`area_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for user
@@ -123,15 +123,15 @@ CREATE TABLE `reservation`  (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `user_ID` int(11) NOT NULL,
-  `username` varchar(32) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
-  `password` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
-  `name` varchar(20) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `sex` int(2) NULL DEFAULT NULL,
-  `phonenumber` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `phonenumber` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `price_status` int(2) NULL DEFAULT NULL,
-  `user_pic` varchar(512) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `user_pic` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `del_status` int(2) NULL DEFAULT NULL,
   PRIMARY KEY (`user_ID`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
